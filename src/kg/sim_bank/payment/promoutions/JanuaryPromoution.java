f4c2bd5bd451bf08472b6af8199046e4477cff25
minarakudus <minara7k@gmail.com>
2026-01-24 09:54:50 +0600

    Registration v3
    January promoution1
    
    January promoution2
    
    January promoution5
    
    January promoution7
    
    January promoution3

---
+++ src/kg/sim_bank/payment/promoutions/JanuaryPromoution.java
@@ -0,0 +1,18 @@
+package kg.sim_bank.payment.promoutions;
+
+public class JanuaryPromoution {
+    static void main(String[] args) {
+
+
+        System.out.println("January");
+        System.out.println("January123");
+        System.out.println("January1234");
+        System.out.println("January12345");
+        System.out.println("January123456");
+        System.out.println("January1234567");
+        System.out.println("January12345678");
+        System.out.println("January123456789");
+        System.out.println("January1234567890");
+        System.out.println("January12345678901");
+    }
+}
