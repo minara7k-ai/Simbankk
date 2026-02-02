7359a5864a8660963da78953f00fe3842f6751be
minarakudus <minara7k@gmail.com>
2026-02-02 21:17:03 +0600

    KoverSamolet

--- src/kg/sim_bank/payment/delivery/Delivery.java
+++ src/kg/sim_bank/payment/delivery/Delivery.java
@@ -4,5 +4,7 @@ public class Delivery {
     static void main(String[] args) {
 
         System.out.println("Delivery Yandex");
+        System.out.println("Delivery Glovo");
+        System.out.println("Delivery KoverSamolet");
     }
 }
