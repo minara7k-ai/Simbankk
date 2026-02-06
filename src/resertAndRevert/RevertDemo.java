55d737c79c97abf3f51578c47a312f2f0e5b420b
minarakudus <minara7k@gmail.com>
2026-02-06 09:47:51 +0600

    Revert "My name"
    This reverts commit 115da491696fdc8713632f97061913a74d8106fa.

--- src/resertAndRevert/RevertDemo.java
+++ src/resertAndRevert/RevertDemo.java
@@ -3,6 +3,5 @@ package resertAndRevert;
 public class RevertDemo {
     static void main(String[] args) {
         System.out.println("Demo1");
-        System.out.println(My name is Minara);
     }
 }
