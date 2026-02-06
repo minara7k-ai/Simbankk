115da491696fdc8713632f97061913a74d8106fa
minarakudus <minara7k@gmail.com>
2026-02-06 09:41:06 +0600

    My name

--- src/resertAndRevert/RevertDemo.java
+++ src/resertAndRevert/RevertDemo.java
@@ -3,5 +3,6 @@ package resertAndRevert;
 public class RevertDemo {
     static void main(String[] args) {
         System.out.println("Demo1");
+        System.out.println(My name is Minara);
     }
 }
